d1ba684ea1bdc1cf5a1681ed97c17b8957f0ccdc
TSephiri <31255086@student.g.nwu.ac.za>
2020-10-09 09:44:26 +0100

    created process classs

---
+++ FCFS/Process.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCFS
+{
+    class Process
+    {
+
+        public string name { get; set; }
+        public int burstTime{get;set;}
+        public int arrivalTime { get; set; }
+        public int priority { get; set; }
+
+    }
+}
